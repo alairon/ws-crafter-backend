8f1b2ae985cfb66ec825b39eff6eb219e1e6176a
Ambrose <alairon@outlook.com>
2020-02-23 22:23:10 -0500

    Updated functions and added Date validation

--- src/dataValidation.js
+++ src/dataValidation.js
@@ -1,10 +1,20 @@
-exports.isNull = function (object) {
+function isNull (object) {
   if (object === null) return true;
   return false;
 }
 
-exports.isUndefined = function (object) {
+function isUndefined (object) {
   if (typeof (object) === typeof (undefined)) return true;
+  return false;
+}
+
+exports.isEmpty = function (object) {
+  if (isNull(object) || isUndefined(object)) return true;
+  return false;
+}
+
+exports.isObject = function (object) {
+  if (typeof (object) === typeof ({})) return true;
   return false;
 }
 
@@ -19,7 +29,23 @@ exports.isBoolean = function (object) {
   return false;
 }
 
-exports.isString = function (object) {
+function isString (object) {
   if (typeof (object) === typeof ('string')) return true;
   return false;
 }
+
+exports.isValidDate = function (object) {
+  const dateFormat = /\d{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])/gm;
+  if (isString (object)) {
+    //Stop if the string is not in YYYY-MM-DD or fails a simple date check (e.g. MM > 12, DD > 31)
+    if (isNull(dateFormat.exec(object))) {
+      return false;
+    } 
+    return true;
+  }
+  return false;
+}
+
+exports.isNull = isNull;
+exports.isUndefined = isUndefined;
+exports.isString = isString;
