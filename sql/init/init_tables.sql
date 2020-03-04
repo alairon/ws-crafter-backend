f7d1955b2b31a2bf0127746b3617b321d9b12dfe
Ambrose Lai <alairon@outlook.com>
2020-03-04 18:41:07 -0500

    Update init_tables.sql
    Added a line to match the database documentation

--- sql/init/init_tables.sql
+++ sql/init/init_tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS meta(
     set_name varchar(64) NOT NULL,
     set_number smallint,
     set_side boolean,
+    series_set varchar(8),
     total_cards smallint NOT NULL,
     release_date date NOT NULL,
     UNIQUE (set_id),
