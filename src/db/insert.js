7f7b20eca8424b5d1564cec1902c404172aa0ee1
alairon <alairon@outlook.com>
2020-03-15 19:01:03 -0400

    Integrated validation and parsing of files

--- src/db/insert.js
+++ src/db/insert.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-restricted-syntax */
 const mysql = require('mysql');
-const mysqlConfig = require('../sql/credentials.json');
+const mysqlConfig = require('../../sql/credentials.json');
+const { readFile } = require('fs');
 const { importCardList } = require('../files/getContents');
+const jsonValidation = require('../validation/jsonValidation');
 
 const connection = mysql.createConnection({
   host: mysqlConfig.write.host,
@@ -16,13 +18,44 @@ function addToDB(query){
   return 0;
 }
 
-function insert(setRootDir) {
+function insert(rootDir){
   const query = [];
-  const fileList = importCardList(setRootDir);
+  const fileList = importCardList(rootDir);
+  let dir = rootDir;
 
-  console.log(fileList);
+  for (let i in fileList){
+    //File being read is meta.json
+    if (fileList[i] == 'meta.json'){
+      dir = rootDir;
+      readFile((dir + fileList[i]), 'utf8', (err, contents) => {
+        if (err) throw err.code;
+        const meta = JSON.parse(contents);
 
-  return 0;
+        if (jsonValidation.metaCheck(meta)) {
+          query.push(meta);
+        }
+        else {
+          query.push(null);
+        }
+      });
+    //Anything else that isn't meta.json
+    } else {
+      dir = rootDir + '/cards/';
+      readFile((dir + fileList[i]), 'utf8', (err, contents) => {
+        if (err) throw err.code;
+
+        const data = JSON.parse(contents);
+        jsonValidation.cardValidation(data);
+        query.push(data);
+      });
+    }
+
+    // Send the validated data to addToDB to add the contents into the DB
+  }
+
+  // Return an error code rather than the query itself. This function will be called from the router
+  return query;
+
 }
 
 exports.insert = insert;
