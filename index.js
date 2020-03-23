70eb9c5815333a126a226bf54fc2235f3c58fbea
alairon <alairon@outlook.com>
2020-03-22 21:41:09 -0400

    express now receives JSON data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-/* eslint no-console: 0 */
 
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
+const bodyParser = require('body-parser');
 const mysqlConfig = require('./sql/credentials.json');
 
 const connection = mysql.createConnection({
@@ -22,19 +22,28 @@ const readReq = (res) => {
   let query = 'SELECT * from cards_general';
   connection.query(query, (err, results) => {
     if (err) res.status(404).send(`There was an error with your request: ${err.sqlMessage}`);
-    res.send(results);
+    res.json(results);
   });
 };
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use('/insert', express.static(path.join(__dirname, 'src/tools/')));
 
 app.get('/', (req, res) => {
   console.log('Session requested');
   res.send('Connection Successful');
 });
 
-app.use('/insert', express.static(path.join(__dirname, 'src/tools/')));
 app.get('/dev/insert', (req, res) => {
-  console.log('Writing Requested');
-  res.sendFile(path.join(__dirname, 'src/tools', 'dataCreation.html'));
+  console.log('[INSERT] Database insertion form requested');
+  res.sendFile(path.join(__dirname, 'src/tools', 'index.html'));
+});
+
+app.post('/insert', (req, res) => {
+  console.log('[INSERT] Database file received');
+  console.log(req.body);
+  res.send('Request acknowledged');
 });
 
 app.get('/test', (req, res) => {
