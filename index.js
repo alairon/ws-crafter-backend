b448a6b0ba5ff39e81a25dad0f362639613ad29e
alairon <alairon@outlook.com>
2020-03-27 00:23:44 -0400

    Updated server paths

--- index.js
+++ index.js
@@ -27,36 +27,68 @@ const readReq = (res, query) => {
 };
 
 app.use(bodyParser.json());
-app.use('/insert', express.static(path.join(__dirname, 'src/tools/')));
 
+/* ROOT PAGE: Home page if no parameters are sent */
 app.get('/', (req, res) => {
   console.log(`[HOME] Request ${count += 1}`);
   res.send('Connection Successful');
 });
 
-app.get('/api/insert', (req, res) => {
+/* PAGE: Delivers a page for the user to input card data */
+app.use('/insert', express.static(path.join(__dirname, 'src/tools/')));
+app.get('/insert', (req, res) => {
   console.log(`[INSERT] Request ${count += 1}`);
   res.sendFile(path.join(__dirname, 'src/tools', 'index.html'));
 });
 
+/* POST: Receives data to be inserted onto the server */
 app.post('/insert', cors(), (req, res) => {
   console.log(`[INSERT] Request ${count += 1}`);
   createFile(req.body[0]);
   res.send('Request complete');
 });
 
-app.get('/cards', (req, res) => {
+/* API: Gets list of sets available on the server */
+app.get('/api/cards/', (req, res) => {
   console.log(`[CARDS] Request ${count += 1}`);
-  const query = 'SELECT * from meta JOIN cards_general ON meta.set_id = cards_general.set_id';
+  const query = 'SELECT set_id, set_name FROM meta';
   readReq(res, query);
 });
 
-app.get('/dump', (req, res) => {
-  console.log(`[DUMP] Request ${count += 1}`);
-  const query = 'SELECT * from meta';
+/* API: Gets list of cards from a specific set */
+app.get('/api/cards/:set', (req, res) => {
+  const set = req.params.set;
+  console.log(`[CARDS] Request ${count += 1}`);
+  const query = `SELECT * FROM cards_general WHERE set_id='${set}'`;
   readReq(res, query);
-})
+});
 
+/* API: Gets a detailed list of character cards from a specific set */
+app.get('/api/cards/:set/character', (req, res) => {
+  const set = req.params.set;
+  console.log(`[CARDS] Request ${count += 1}`);
+  const query = `SELECT * FROM cards_general JOIN cards_character ON cards_general.card_id = cards_character.card_id WHERE set_id='${set}'`;
+  readReq(res, query);
+});
+
+/* API: Gets a detailed list of event cards from a specific set */
+app.get('/api/cards/:set/event/', (req, res) => {
+  const set = req.params.set;
+  console.log(`[CARDS] Request ${count += 1}`);
+  const query = `SELECT * FROM cards_general JOIN cards_event ON cards_general.card_id = cards_event.card_id WHERE set_id='${set}'`;
+  readReq(res, query);
+});
+
+/* API: Gets a detailed list of climax cards from a specific set */
+app.get('/api/cards/:set/climax/', (req, res) => {
+  const set = req.params.set;
+  console.log(`[CARDS] Request ${count += 1}`);
+  const query = `SELECT * FROM cards_general JOIN cards_climax ON cards_general.card_id = cards_climax.card_id WHERE set_id='${set}'`;
+  readReq(res, query);
+});
+
+/* CORE: Alerts the server console that it is ready */
 app.listen((port), () => {
   console.log(`Listening to port: ${port}`);
+  console.log('Ready');
 });
