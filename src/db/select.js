a4a6fb61b5535b1553d35071e60f0754a811462a
alairon <alairon@outlook.com>
2020-04-27 17:29:49 -0400

    Updated SELECTs for use with Postgres

--- src/db/select.js
+++ src/db/select.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
-const mysql = require('mysql');
+const { Pool } = require('pg');
 
-const connection = mysql.createConnection({
+const connection = new Pool({
   host: process.env.DB_READ_HOST,
   user: process.env.DB_READ_USER,
   password: process.env.DB_READ_PASSWORD,
   database: process.env.DB_READ_DATABASE,
 });
 
+connection.connect((err) => {
+  if (err){
+    console.error(err);
+  }
+  else{
+    console.log('Database successfully connected.');
+  }
+});
+
 const readReq = (res, query) => {
   connection.query(query, (err, results) => {
-    if (err) res.status(404).send(`There was an error with your request: ${err.sqlMessage}`);
+    if (err) res.status(400).send(`There was an error with your request: ${err}`);
     if (results == {}) res.status(204).json(results);
-    res.json(results);
+    res.json(results.rows);
   });
 };
 
